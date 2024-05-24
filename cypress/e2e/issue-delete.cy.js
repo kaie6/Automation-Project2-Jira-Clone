e961a2dee0c077b1c55c79a0f74c19774ec8287c
kaie6 <kaie666@gmail.com>
2024-05-24 21:16:36 +0300

    Assignment#3 (ed#2)

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -8,6 +8,7 @@ const conformationWindow = '[data-testid="modal:confirm"]';
 const closeButtons = '[data-testid="icon:close"]';
 const message1 = "Are you sure you want to delete this issue?";
 const message2 = "Once you delete, it's gone for good.";
+let firstIssueTitle;
 
 function ConfirmationWindowActions(buttonTitle) {
   cy.get(conformationWindow)
@@ -56,11 +57,11 @@ describe("Issue Deletion", () => {
 
   it("Should delete the first issue in the backlog column", () => {
     let deletedIssueCount = 1;
-    let firstIssueTitle;
 
+    //Get the name of the first issue for future assertions
     cy.get(singleIssue).first().invoke("text").as("firstIssueTitle");
-
     cy.get("@firstIssueTitle").then((text) => {
+      //Save the name as a variable
       firstIssueTitle = text;
       cy.get(singleIssue).first().click();
       cy.get(IssueDetailsModal).within(() => {
@@ -70,21 +71,20 @@ describe("Issue Deletion", () => {
       });
 
       ConfirmationWindowActions("Delete issue");
+
       cy.get(IssueDetailsModal).should("not.exist");
+
       BacklogAssertions(deletedIssueCount, firstIssueTitle);
     });
   });
 
   it("Should not delete the issue when user cancels its deletion", () => {
     let deletedIssueCount = 0;
-    let firstIssueTitle;
 
     cy.get(singleIssue).first().invoke("text").as("firstIssueTitle");
-
     cy.get("@firstIssueTitle").then((text) => {
       firstIssueTitle = text;
       cy.get(singleIssue).first().click();
-
       cy.get(IssueDetailsModal).within(() => {
         cy.get(issueTitle).should("contain", firstIssueTitle);
         cy.log("First issue title is: " + firstIssueTitle);
@@ -97,8 +97,8 @@ describe("Issue Deletion", () => {
         .should("be.visible")
         .and("contain", firstIssueTitle);
       cy.get(closeButtons).first().click();
-
       cy.get(IssueDetailsModal).should("not.exist");
+
       BacklogAssertions(deletedIssueCount, firstIssueTitle);
     });
   });
